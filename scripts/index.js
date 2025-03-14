ce11b4257a3722df9306badcdf3b3edf133067a7
smabari <mdazizulbarishafin@gmail.com>
2025-03-15 02:12:02 +0600

    added categories

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
 const loadCategories = () => {
+  //   fetch the data
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
-  .then((response) => response.json())
-  .then((data)=> displayCategories(data.categories)) 
+    //   convert promise to json
+    .then((response) => response.json())
+    //   send data to display
+    .then((data) => displayCategories(data.categories));
 };
-const displayCategories = (categories)=> {
-    console.log(categories)
-}
-loadCategories()
+const displayCategories = (categories) => {
+  // get the container
+  const categoryContainer = document.getElementById("category-container");
+  // loop operation on array of object
+  for (let category of categories) {
+    console.log(category);
+    // create element
+    const categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML = `
+
+      <button class="btn btn-sm hover:bg-red-500 hover:text-white">${category.category}</button>
+    `;
+    // append element
+    categoryContainer.append(categoryDiv);
+  }
+};
+loadCategories();
